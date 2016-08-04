be1344456d99d2aa4e3752951a7abf30292e561d
amoulis <none>
2016-08-04 14:32:22 +0200

    Printer class declaration

---
+++ src/Printer.cpp
@@ -0,0 +1,8 @@
+#include "Printer.hpp"
+namespace printer{
+Printer::Printer()
+  : length(8)
+  , width(24)
+{};
+
+}
---
+++ src/Printer.hpp
@@ -0,0 +1,18 @@
+#ifndef PRINTER_HPP
+#define PRINTER_HPP
+
+namespace printer{
+class
+Printer {
+
+private:
+  int length;
+  int width;
+  Printer();
+  int print_screen();
+
+};
+
+} // namesoace printer
+
+#endif //Printer_HPP
