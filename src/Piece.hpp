09a2bbbbe86739e210c1bc8c3738c12a0e8f0272
amoulis <adrien.moulis@gmail.com>
2016-08-05 11:19:27 +0200

    Piece heritage

--- src/Piece.hpp
+++ src/Piece.hpp
@@ -9,10 +9,42 @@ class
 Piece
 {
   protected:
+  int x;
+  int y;
   virtual std::vector <std::vector <int> > get_next_pos(int i, int j);
   virtual std::vector <int> get_current_pos();
   virtual void change_current_pos(int i, int j); 
    
+};
+
+class
+Knight : public Piece {
+
+};
+
+class
+Rook : public Piece {
+
+};
+
+class
+Bishop : public Piece {
+
+};
+
+class
+Pawn : public Piece {
+
+};
+
+class
+King : public Piece {
+
+};
+
+class
+Queen : public Piece {
+
 };
 
 } // namespace piece
