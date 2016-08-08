51e6a287408ed15b8775adf2649cf399568e258b
amoulis <adrien.moulis@gmail.com>
2016-08-08 15:08:44 +0200

    Game class files

---
+++ src/Game.hpp
@@ -0,0 +1,19 @@
+/******************************************************************************
+Game.hpp
+All the game 'mechanic' : legit move checking, check (mate) detection, 
+piece captures detection...
+******************************************************************************/
+#ifndef GAME_HPP
+#define GAME_HPP
+
+namespace game {
+
+class
+Game
+{
+
+};
+
+} // namespace Game
+
+#endif // GAME_HPP
