7a31a79ee15eef25037ba316918c6a6f603caf6c
amoulis <adrien.moulis@gmail.com>
2016-08-08 15:26:26 +0200

    is_out_of_the_board_function

--- src/Game.cpp
+++ src/Game.cpp
@@ -2,4 +2,23 @@
 
 namespace game {
 
+
+std::vector <std::vector <int> >
+Game::is_not_out_of_the_board(std::vector < std::vector <int> >& data_in)
+{
+  for(int i = 0; i < data_in.size(); i++)
+  {
+    std::vector<int> vec = data_in[i];
+    if( vec[0] < 0 || vec[0] > m_board_size)
+    {
+      if( vec[1] < 0 || vec[1] > m_board_size)
+      {
+        data_in.erase(data_in.begin()+i);
+      }
+    }
+  }    
+
+  return data_in;
+}
+
 } // namespace game
--- src/Game.hpp
+++ src/Game.hpp
@@ -6,12 +6,19 @@ piece captures detection...
 #ifndef GAME_HPP
 #define GAME_HPP
 
+#include <vector>
+
 namespace game {
 
 class
 Game
 {
-
+  public:
+   
+  private:
+  static const unsigned int m_board_size = 7;
+  std::vector <std::vector <int> > is_not_out_of_the_board(std::vector < std::vector <int> >& data_in);
+  
 };
 
 } // namespace Game
